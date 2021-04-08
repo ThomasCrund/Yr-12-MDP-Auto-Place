985744e04e51039215697eeaaea06f0663752f5b
ThomasCrund <39323104+ThomasCrund@users.noreply.github.com>
2021-04-08 11:26:09 +1000

    Started Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MaterialImporter from './component/MaterialImporter'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Auto Place</h1>
+      <MaterialImporter />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/component/MaterialImporter.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export class MaterialImporter extends Component {
+    componentDidMount() {
+        // import('../opencv/opencv').then(opencv => {
+        //     console.log(opencv)
+        // })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h3>Material Detecter</h3>
+            </div>
+        )
+    }
+}
+
+export default MaterialImporter
