cedbe70598dfd04f36d7d531f241e4ca1b232cb1
ThomasCrund <39323104+ThomasCrund@users.noreply.github.com>
2021-04-12 08:33:16 +1000

    Added blur to image

--- frontend/src/component/MaskCreator.js
+++ frontend/src/component/MaskCreator.js
@@ -1,32 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import { useOpenCv } from 'opencv-react'
 
+
 export default function MaskCreator(props) {
     const [ colours, setColours] = useState([]);
+    const [ blur, setBlur] = useState(0);
     const { cv } = useOpenCv();
 
     useEffect(() => {
         if (cv) {
-            cv.imshow('canvasOutput', props.img);
+            let Img = new cv.Mat()
+            let ksize = new cv.Size(blur, blur);
+            if (blur !== 0) {
+                // cv.blur(props.img, Img, ksize)
+                // cv.GaussianBlur(props.img, Img, ksize, 0, 0, cv.BORDER_DEFAULT)
+                // cv.GaussianBlur(props.img, Img, ksize, 0, 0, cv.BORDER_DEFAULT);
+                cv.medianBlur(props.img, Img, 1 + Math.round(blur / 2) * 2 );
+            } else {
+                Img = props.img;
+            }
+            // cv.imshow('canvasOutput', props.img);
+            cv.imshow('canvasBlurred', Img);
             let mask = cv.Mat.zeros(props.img.rows, props.img.cols, 0);
             for (let i = 0; i < colours.length; i++) {
                 const element = colours[i];
                 let dst = new cv.Mat()
                 let low = new cv.Mat(props.img.rows, props.img.cols, props.img.type(), [element[0]-10, element[1]-10, element[2]-10, 0]);
                 let high = new cv.Mat(props.img.rows, props.img.cols, props.img.type(), [element[0]+10, element[1]+10, element[2]+10, 255]);
-                cv.inRange(props.img, low, high, dst);
+                cv.inRange(Img, low, high, dst);
                 cv.add(mask, dst, mask);
                 low.delete();
                 high.delete();
                 dst.delete()
             }
             cv.imshow('canvasMask', mask);
-            
+            // Img.delete();
         }
-    }, [cv, props.img, colours])
+    }, [cv, props.img, colours, blur])
 
     let canvasClick = (e) => {
-        console.log(props.img)
+        // console.log(props.img)
         let xPos = e.clientX - e.target.offsetLeft
         let yPos = e.clientY - e.target.offsetTop
         let B = props.img.data[yPos * props.img.cols * props.img.channels() + xPos * props.img.channels() + 2];
@@ -37,7 +50,7 @@ export default function MaskCreator(props) {
             for (let i = 0; i < arrayExisting.length; i++) {
                 found = (arrayExisting[i][0] === R && arrayExisting[i][1] === G && arrayExisting[i][2] === B) ? true : found
             }
-            console.log(found)
+            // console.log(found)
             if ( !found ) {
                 return [[R, G, B], ...arrayExisting ]
             }
@@ -53,7 +66,11 @@ export default function MaskCreator(props) {
 
     return (
         <div>
-            <canvas id="canvasOutput" onClick={canvasClick} ></canvas>
+            <div>
+                Blur: <input type="range" min={0} max={9} value={blur} onChange={e => setBlur(parseInt(e.target.value))} /> {blur}
+            </div>
+            {/* <canvas id="canvasOutput" onClick={canvasClick} ></canvas> */}
+            <canvas id="canvasBlurred" onClick={canvasClick} ></canvas>
             <div>{JSON.stringify(colours, 2)}</div>
             <canvas id="canvasMask" onClick={canvasClick} ></canvas>
         </div>
